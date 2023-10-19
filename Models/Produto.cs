c03c44d5b8cde40971fc66514ea09f05d66662f2
thiagobcf <thiagobcfonseca@gmail.com>
2023-10-19 18:26:07 -0300

    add class categoria e produto

---
+++ Models/Produto.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIcatalogo.Models
+{
+    public class Produto
+    {
+        public int ProdutoId { get; set; }
+        public string? Nome { get; set; }
+        public string? Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public string? ImagemUrl { get; set; }
+        public float Estoque { get; set; }
+        public DateTime DataCadastro { get; set; }
+
+    }
+}

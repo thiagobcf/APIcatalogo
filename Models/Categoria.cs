aaa8a8c487b10610baa0d59f97667752e0b439a2
thiagobcf <thiagobcfonseca@gmail.com>
2023-10-23 10:58:39 -0300

    add Data Annotations

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -3,17 +3,29 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+
 
 namespace APIcatalogo.Models
-{
+{   
+    [Table("Categorias")]
     public class Categoria
     {
         public Categoria()
         {
             Produtos = new Collection<Produto>();
         }
+        [Key]
         public int CategoriaId { get; set; }
+        [Required]
+        [StringLength(80)]
+
         public string? Nome { get; set; }
+        [Required]
+        [StringLength(300)]
+        
         public string? ImagemUrl { get; set; }
         public ICollection<Produto>? Produtos { get; set; }
     }

4a1c84a58b697187c600966ed7d52981411ef5dd
thiagobcf <thiagobcfonseca@gmail.com>
2023-10-24 17:01:02 -0300

    PUT update the product

--- Controllers/ProdutosController.cs
+++ Controllers/ProdutosController.cs
@@ -29,7 +29,8 @@ namespace APIcatalogo.Controllers
             }
             return produtos;
         }
-        [HttpGet("{id:int}")]
+
+        [HttpGet("{id:int}", Name="ObterProduto")]
         public ActionResult<Produto> Get(int id)
         {
             var produto = _context.Produtos.FirstOrDefault(p=> p.ProdutoId == id);
@@ -39,5 +40,18 @@ namespace APIcatalogo.Controllers
             }
             return produto;
         }
+
+        [HttpPost]
+        public ActionResult Post(Produto produto)
+        {
+            if (produto is null)
+                return BadRequest();
+                
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutoId }, produto);
+        }
     }
 }
+
